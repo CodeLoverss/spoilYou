5728326ec37a45927ce9b37ab325b232ea79eb5c
CodeLoverss <44800106+CodeLoverss@users.noreply.github.com>
2020-07-23 11:45:39 +0800

    合并

--- src/main/java/com/ytz/spoilyou/controller/ManagerController.java
+++ src/main/java/com/ytz/spoilyou/controller/ManagerController.java
@@ -6,23 +6,18 @@ import com.ytz.spoilyou.service.PetService;
 import com.ytz.spoilyou.service.UserService;
 import com.ytz.spoilyou.util.HandlePhoto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.util.List;
 
-import static com.ytz.spoilyou.constant.Constant.FALSE;
-import static com.ytz.spoilyou.constant.Constant.SUCCESS;
 
 //对pet和用户 CRUD
 @Controller
